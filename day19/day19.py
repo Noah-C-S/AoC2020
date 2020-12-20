f3d9a9f3c8665df9a494aaea21d423c6123b5b8c
Noah-C-S <noahcscs@gmail.com>
2020-12-20 09:05:40 -0500

    Day 20!

--- day19/day19.py
+++ day19/day19.py
@@ -32,13 +32,8 @@ for line in inputFile:
         continue
     if(mode == 0):
         ruleNum = int(line.strip().partition(":")[0])
-        #rule = line.partition(": ")[2].split("|")
         rule = line.strip().partition(": ")[2].strip()
         rules[ruleNum] = rule
-        #print(line.strip())
-        #print(str(ruleNum) + ": " + rules[ruleNum])
-        """for i in range(len(rule)):
-            rule[i] = rule[i].strip()"""
     elif(mode == 1):
         messages.append(line.strip())
 
@@ -50,94 +45,6 @@ for message in messages:
 
 log(rules)
 #log(messages)
-
-
-#Expands a rule to a list of possible strings of as and bs
-"""def expandRule(ruleNum):
-    print(depth)
-    rule = rules[ruleNum]
-    expandedRule = []
-    log("ExpandRule")
-    print(rule)
-    for r in rule:
-        divis = r.split(" ")
-        print(divis)
-        try:
-            one = int(divis[0])
-        except ValueError:
-            one = divis[0].replace('"', '')
-            oneExpanded = []
-            oneExpanded.append(one)
-        else:
-            oneExpanded = expandRule(one)
-        try:
-            two = int(divis[1])
-        except ValueError:
-            two = divis[1].replace('"', '')
-            twoExpanded = []
-            twoExpanded.append(two)
-        except IndexError:
-            two = ''
-            twoExpanded = []
-            twoExpanded.append(two)
-        else:
-            twoExpanded = expandRule(two)
-        log(str(oneExpanded) + "\n" + str(twoExpanded) + "\n__________")
-        for e1 in oneExpanded:
-            for e2 in twoExpanded:
-                expandedRule.append(e1 + e2)
-                
-    return expandedRule"""
-
-def expandRule(ruleNum):
-    rule = rules[ruleNum]
-    expandedRule = rule
-    log("ExpandRule")
-    print(rule)
-    done = False
-    while not done:
-        done = True
-        nextIter = []
-        for r in expandedRule:
-            divis = r.split(" ")
-            for i in range(len(divis)):
-                if(divis[i] in ["a", "b"]):
-                    continue
-                elif(divis[i] in ['"a"', '"b"']):
-                    divis[i] 
-            try:
-                one = int(divis[0])
-            except ValueError:
-                one = divis[0].replace('"', '', 1).replace('"', ' ')
-                oneExpanded = []
-                oneExpanded.append(one)
-            else:
-                ruleOne = rules[one]
-                #print("one: " + str(one))
-                oneExpanded = ruleOne
-                done = False
-            try:
-                two = int(divis[1])
-            except ValueError:
-                two = divis[1].replace('"', '', 1).replace('"', ' ')
-                twoExpanded = []
-                twoExpanded.append(two)
-            except IndexError:
-                two = ''
-                twoExpanded = []
-                twoExpanded.append(two)
-            else:
-                ruleTwo = rules[two]
-                twoExpanded = ruleTwo
-                done = False
-            #log(str(oneExpanded) + "\n" + str(twoExpanded) + "\n__________")
-            for e1 in oneExpanded:
-                for e2 in twoExpanded:
-                    nextIter.append(e1 + " " + e2)
-            log(nextIter)
-        expandedRule = nextIter
-                
-    return expandedRule
 
 def createRegEx(ruleNum):
     rule = rules[ruleNum]
@@ -203,12 +110,9 @@ def part2():
     rules[31] = " " + createRegEx(31) + " "
     
     for i in range(2, messLen):
-        #rules[11] = rules[11] + " | " + ((" " + rules[42] + " ")) * i + ((" " + rules[31] + " ") * i)
         rules[11] = rules[11] + " | " + ((" 42 ")) * i + ((" 31 ") * i)
     rules[11] = createRegEx(11)
     #print(createRegEx(11))
-    
-    #sys.exit(0)
     expr = createRegEx(0)
     log(expr)
     for message in messages:
